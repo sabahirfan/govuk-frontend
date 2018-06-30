33a699fb2cc52ea24fcb2afaddac0e528bafaf6e
igloosi <jani.kraner@gmail.com>
2018-06-30 21:27:39 +0100

    Allow for optional divider between radio items

--- src/components/radios/template.test.js
+++ src/components/radios/template.test.js
@@ -224,6 +224,33 @@ describe('Radios', () => {
       expect($lastConditional.attr('id')).toBe('conditional-example-conditional-2')
       expect($lastConditional.html()).toContain('Conditional content')
     })
+
+    it('render divider', () => {
+      const $ = render('radios', {
+        name: 'example-divider',
+        items: [
+          {
+            value: 'gateway',
+            text: 'Sign in with Government Gateway'
+          },
+          {
+            value: 'verify',
+            text: 'Sign in with GOV.UK Verify'
+          },
+          {
+            divider: 'or'
+          },
+          {
+            value: 'account',
+            text: 'Create an account'
+          }
+        ]
+      })
+
+      const $component = $('.govuk-radios')
+      const $divider = $component.find('.govuk-radios__divider')
+      expect($divider.text()).toBe('or')
+    })
   })
 
   describe('when they include a hint', () => {
